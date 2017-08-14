257bd8d567b0f1198c1da88b214f621ecf70de64
wupeiyin <1136970917@qq.com>
2017-08-14 13:59:47 +0800

    第二次提交 2017-8-14 13:59:37

---
+++ app/src/main/java/com/example/myday03/Demo.java
@@ -0,0 +1,13 @@
+package com.example.myday03;
+
+import android.util.Log;
+
+/**
+ * Created by 勿忘初心 on 2017/8/14.
+ */
+
+public class Demo {
+    public void sho(){
+        Log.e("TAG", "day03");
+    }
+}
